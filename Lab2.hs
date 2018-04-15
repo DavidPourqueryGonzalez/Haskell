cbea5a7cff51bf30a9139dcc36629d9c2ecb65a1
cupcakes3000 <zcabpou@ucl.ac.uk>
2018-04-15 18:31:13 +0100

    doing lab2

---
+++ Lab2.hs
@@ -0,0 +1,28 @@
+import Data.Char
+
+inRange :: Int -> Int -> [Int] -> [Int]
+inRange _ _ [] = []
+inRange a b (x : xs) = if x >= a && x <=b then x : inRange a b xs
+                       else inRange a b xs
+
+countPositives :: [Int] -> Int
+countPositives [] = 0
+countPositives (x : xs) = if x > 0 then 1 + countPositives xs
+                          else countPositives xs
+
+insert :: Ord a => a -> [a] -> [a]
+insert a [] = [a]
+insert a (x : xs) = if a <= x then a : x : xs
+                    else x : insert a xs
+
+isort :: Ord a => [a] -> [a]
+isort [] = []
+isort (x : xs) = insert x (isort xs)
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x : xs) (y : ys) = if x < y then x : merge xs (y : ys)
+                          else y : merge (x : xs) ys
+
+msort :: Ord a => [a] -> [a]
