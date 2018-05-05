5a1477d0443ed14b22ee3c81fffc7234e6b2a13e
cupcakes3000 <zcabpou@ucl.ac.uk>
2018-05-05 18:23:48 +0100

    nearly done lab3

--- Lab3.hs
+++ Lab3.hs
@@ -1,2 +1,30 @@
 mult :: Num a => [a] -> a
 mult xs = foldr (*) 1 xs
+
+posList :: [Int] -> [Int]
+posList xs = filter(\n -> n > 0) xs
+
+trueList :: [Bool] -> Bool
+trueList bs = foldr (&&) True bs
+
+evenList :: Integral a => [a] -> Bool
+evenList xs = trueList ( map (\n -> even n) xs )
+
+maxList :: (Ord a) => [a] -> a
+maxList [] = error "no max"
+maxList [x] = x
+maxList (x:xs)
+        | x > maximum xs = x
+        | otherwise = maximum xs
+   
+inRange :: Int -> Int -> [Int] -> [Int]
+inRange a b xs = [ x | x<-xs, x >= a, x <= b]
+
+countPositives :: [Int] -> Int 
+countPositives xs = length $ posList xs
+
+myLength :: [a] -> Int
+myLength xs = foldr (+) 0 $ map (\n -> 1) xs
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f (x:xs) = foldr (\y ys -> (f y):ys) [] xs
